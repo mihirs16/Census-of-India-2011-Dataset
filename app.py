bd7e128555abc30c4dee534e5d93864ea4d91683
Mihir Singh <mihirs16@gmail.com>
2020-12-31 13:40:22 +0530

    Temporary Client

---
+++ app.py
@@ -0,0 +1,47 @@
+# imports
+import data
+import os, dotenv
+import flask, flask_cors
+
+# server setup
+app = flask.Flask(__name__, static_folder="dashboard/build/static", template_folder="dashboard/build")
+flask_cors.CORS(app, support_credentials = True)
+
+# database setup
+dotenv.load_dotenv()
+username = os.getenv("SQL_USER")
+password = os.getenv("SQL_PASS")
+hostname = os.getenv("SQL_HOST")
+db = data.Database("may_db", username, password, hostname)
+
+# default route | returns homepage
+@app.route("/", methods = ["GET"])
+def home():
+    return flask.render_template("index.html")
+
+# database route | returns filtered table
+@app.route("/database", methods = ["POST"])
+def get_data():
+    received = flask.request.json
+    return db.fetch(
+        table_name = received.get("table_name"), 
+        filter_column = received.get("column_filter"),
+        filter_values = received.get("filter_values")
+    ).to_json()
+
+# app run
+if __name__ == '__main__':
+    # for hot reload and tracking static files and templates
+    from os import path, walk
+
+    extra_dirs = ['templates/', 'static/']
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in walk(extra_dir):
+            for filename in files:
+                filename = path.join(dirname, filename)
+                if path.isfile(filename):
+                    extra_files.append(filename)
+
+    # flask app run
+    app.run(debug=True, extra_files=extra_files)
